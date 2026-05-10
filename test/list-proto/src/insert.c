c8fb62c0ba58bc5812783310408c78d70a9be019
Archeus Lam <mrsssprite@gmail.com>
2026-05-10 15:57:12 +0800

    feat(list-proto): assert head and size after init

--- test/list-proto/src/insert.c
+++ test/list-proto/src/insert.c
@@ -23,6 +23,10 @@ void test_insert_head_seq(size_t sz)
 {
    List__i list = list__i_init();
    TEST_ASSERT_MESSAGE(list, "`list__i_init' failure");
+   TEST_ASSERT_NULL_MESSAGE(list->head,
+                            "`list->head' not initialized to NULL");
+   TEST_ASSERT_EQUAL_UINT_MESSAGE(0, list->size,
+                                  "`list->size' not initialized to 0");
 
    for (size_t i = 0; i < sz; i++)
       TEST_ASSERT_EQUAL_INT(0, list__i_push(list, i));
