ded9e0560c34f9bad0755c22592757c26ef58a10
Archeus Lam <mrsssprite@gmail.com>
2026-05-10 16:05:55 +0800

    feat(list-proto): add insert_head_random test case
    Add sub-unit test case which is similar to `insert_head_seq' but with
    randomly generated values instead of sequentially incremented integers.

--- test/list-proto/src/insert.c
+++ test/list-proto/src/insert.c
@@ -1,11 +1,14 @@
 /*----------------------------- Private Includes -----------------------------*/
 #include "ds.h"
 #include "unity.h"
+#include <stdlib.h>
+#include <time.h>
 /*--------------------------- Private Includes END ---------------------------*/
 
 
 /*--------------------------- Private Declarations ---------------------------*/
 void test_insert_head_seq(size_t sz);
+void test_insert_head_random(size_t sz);
 /*------------------------- Private Declarations END -------------------------*/
 
 
@@ -13,7 +16,10 @@ void test_insert_head_seq(size_t sz);
 void test_insert_head(void)
 {
    for (size_t list_sz = 1; list_sz <= 0x100000; list_sz *= 2)
+    {
       test_insert_head_seq(list_sz);
+      test_insert_head_random(list_sz);
+    }
 }
 /*------------------------------ Test Units END ------------------------------*/
 
@@ -39,7 +45,39 @@ void test_insert_head_seq(size_t sz)
    for (size_t i = sz; i; iter = &(*iter)->next)
       TEST_ASSERT_EQUAL_INT(--i, (*iter)->value);
 
+   list__i_deinit(list);
+}
 
+
+void test_insert_head_random(size_t sz)
+{
+   size_t i;
+   int *arr = malloc(sizeof(int) * sz);
+   TEST_ASSERT_NOT_NULL(arr);
+   srand(time(NULL));
+   for (i = 0; i < sz; i++)
+      arr[i] = rand();
+
+   List__i list = list__i_init();
+   TEST_ASSERT_MESSAGE(list, "`list__i_init' failure");
+   TEST_ASSERT_NULL_MESSAGE(list->head,
+                            "`list->head' not initialized to NULL");
+   TEST_ASSERT_EQUAL_UINT_MESSAGE(0, list->size,
+                                  "`list->size' not initialized to 0");
+
+   while (i)
+      TEST_ASSERT_EQUAL_INT(0, list__i_push(list, arr[--i]));
+   TEST_ASSERT_EQUAL_size_t(sz, list->size);
+   TEST_ASSERT_NOT_NULL(list->head);
+
+   List_Node__i *iter = list__i_head(list);
+   TEST_ASSERT_NOT_NULL(iter);
+
+   for (i = 0; i < sz; iter = &(*iter)->next)
+      TEST_ASSERT_EQUAL_INT(arr[i++], (*iter)->value);
+   TEST_ASSERT_NULL(*iter);
+
+   free(arr);
    list__i_deinit(list);
 }
 /*------------------------------ Test Cases END ------------------------------*/
