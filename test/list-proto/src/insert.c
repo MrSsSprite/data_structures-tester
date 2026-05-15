3d99eed9353d49be561ce089a73bc1c8073e417a
Archeus Lam <mrsssprite@gmail.com>
2026-05-15 17:47:42 +0800

    test(list-proto/insert): add `head_seq' sub-unit

--- test/list-proto/src/insert.c
+++ test/list-proto/src/insert.c
@@ -10,6 +10,7 @@
 void test_push_seq(size_t sz);
 void test_push_random(size_t sz);
 void test_insert_mid_once(size_t sz);
+void test_insert_head_seq(size_t sz);
 /*------------------------- Private Declarations END -------------------------*/
 
 
@@ -29,6 +30,7 @@ void test_insert(void)
    for (size_t list_sz = 1; list_sz <= 0x100000; list_sz *= 2)
     {
       test_insert_mid_once(list_sz);
+      test_insert_head_seq(list_sz);
     }
 }
 /*------------------------------ Test Units END ------------------------------*/
@@ -139,4 +141,35 @@ void test_insert_mid_once(size_t sz)
 
    list__i_deinit(list);
 }
+
+
+void test_insert_head_seq(size_t sz)
+{
+   TEST_ASSERT_NOT_EQUAL_size_t(0, sz);
+   List__i list = list__i_init();
+   TEST_ASSERT_MESSAGE(list, "`list__i_init' failure");
+   TEST_ASSERT_NULL_MESSAGE(list->head,
+                            "`list->head' not initialized to NULL");
+   TEST_ASSERT_EQUAL_UINT_MESSAGE(0, list->size,
+                                  "`list->size' not initialized to 0");
+
+   int *arr = malloc(sz * sizeof(int));
+   TEST_ASSERT_NOT_NULL(arr);
+   for (size_t i = 0; i < sz; i++)
+      arr[i] = i;
+
+   TEST_ASSERT_EQUAL_INT(0, list__i_insert(list, list__i_head(list), arr, sz));
+   TEST_ASSERT_EQUAL_size_t(sz, list->size);
+   TEST_ASSERT_NOT_NULL(list->head);
+
+   List_Node__i *iter = list__i_head(list);
+   TEST_ASSERT_NOT_NULL(iter);
+
+   for (size_t i = 0; i < sz; i++, iter = &(*iter)->next)
+      TEST_ASSERT_EQUAL_INT(i, (*iter)->value);
+
+   list__i_deinit(list);
+   free(arr);
+}
+
 /*------------------------------ Test Cases END ------------------------------*/
